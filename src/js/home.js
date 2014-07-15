f8ed1c8604b2db49d6cd440a65467123536e46c5
Chiel Kunkels <chiel@kunkels.me>
2014-07-15 15:07:53 +0200

    Stop disabling pointer events on scroll

--- src/js/home.js
+++ src/js/home.js
@@ -18,8 +18,7 @@ var scrollTop = function(){
 var header = $('.header-main'),
 	sections = $('.page'),
 	bodyRect = document.body.getBoundingClientRect(),
-	headers = [], sectionTops = [], i, clone, sectionRect, y,
-	body = document.body, timer;
+	headers = [], sectionTops = [], i, clone, sectionRect, y;
 
 for (i = 0; i < sections.length; i++){
 	clone = $(header[0].cloneNode(true));
@@ -33,16 +32,6 @@ for (i = 0; i < sections.length; i++){
 header.style({display: 'none'});
 
 $(window).on('scroll', function(){
-	clearTimeout(timer);
-
-	if(!body.classList.contains('disable-pointer')) {
-		body.classList.add('disable-pointer');
-	}
-
-	timer = setTimeout(function(){
-		body.classList.remove('disable-pointer');
-	},500);
-
 	y = scrollTop();
 	for (i = 0; i < sections.length; i++){
 		headers[i].style({transform: 'translateY(' + (y - sectionTops[i]) + 'px)'});
