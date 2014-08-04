df62b558912ab07215ee7b7a5618ac5c6e83dc0d
Chiel Kunkels <chiel@kunkels.me>
2014-08-04 16:54:13 +0200

    fix headers being jumpy (hopefully), closes #2

--- src/js/modules/headers.js
+++ src/js/modules/headers.js
@@ -6,25 +6,27 @@ var scrollTop = function(){
 
 var header = $('.header-main'),
 	sections = $('.page'),
-	bodyRect = document.body.getBoundingClientRect(),
-	headers = [], sectionTops = [], i, clone, sectionRect, y;
+	bodyRect, headers = [], sectionTops = [], i, clone, sectionRect, y;
 
-for (i = 0; i < sections.length; i++){
-	clone = $(header[0].cloneNode(true));
-	headers.push(clone);
-	clone.insert(sections[i]);
+$(window).on('load', function(){
+	bodyRect = document.body.getBoundingClientRect();
+	for (i = 0; i < sections.length; i++){
+		clone = $(header[0].cloneNode(true));
+		headers.push(clone);
+		clone.insert(sections[i]);
 
-	sectionRect = sections[i].getBoundingClientRect();
-	sectionTops.push(sectionRect.top - bodyRect.top);
-}
+		sectionRect = sections[i].getBoundingClientRect();
+		sectionTops.push(sectionRect.top - bodyRect.top);
+	}
 
-header.style({display: 'none'});
+	header.style({display: 'none'});
 
-setInterval(function(){
-	window.requestAnimationFrame(function(){
-		y = scrollTop();
-		for (i = 0; i < sections.length; i++){
-			headers[i].style({transform: 'translate3d(0px, ' + Math.round(y - sectionTops[i]) + 'px, 0)'});
-		}
-	});
-}, 10);
+	setInterval(function(){
+		window.requestAnimationFrame(function(){
+			y = scrollTop();
+			for (i = 0; i < sections.length; i++){
+				headers[i].style({transform: 'translate3d(0px, ' + Math.round(y - sectionTops[i]) + 'px, 0)'});
+			}
+		});
+	}, 10);
+});
