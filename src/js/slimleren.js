5ecf9816592f8d5c4953b83fff17ed540fff1e4e
Chiel Kunkels <chiel@kunkels.me>
2014-08-12 17:18:36 +0200

    slimleren: add character development section

--- src/js/slimleren.js
+++ src/js/slimleren.js
@@ -1,7 +1,30 @@
 'use strict';
 
-var timestamp = new Date().getTime(),
+var $ = require('elements'),
+	clamp = require('mout/math/clamp'),
+	easeInOutQuad = require('kubrick-js/easing/easeInOutQuad'),
+	prefix = require('kubrick-js/lib/vendorprefix')(),
+	timestamp = new Date().getTime(),
 	phoneReplaced;
+
+var owls = [
+	{
+		owl: $('.char-owl1 .owl'),
+		wings: $('.char-owl1 .wing-left, .char-owl1 .wing-right')
+	},
+	{
+		owl: $('.char-owl2 .owl'),
+		wings: $('.char-owl2 .wing-left, .char-owl2 .wing-right')
+	},
+	{
+		owl: $('.char-owl3 .owl'),
+		wings: $('.char-owl3 .wing-left, .char-owl3 .wing-right')
+	},
+	{
+		owl: $('.char-owl4 .owl'),
+		wings: $('.char-owl4 .wing-left, .char-owl4 .wing-right')
+	}
+];
 
 require('kubrick-js')([
 	{
@@ -104,6 +127,73 @@ require('kubrick-js')([
 			{
 				element: '.client-clock',
 				translateY: ['-85%', '-180%']
+			},
+			{
+				element: '.char-owl1',
+				translateY: '-85%'
+			},
+			{
+				element: '.char-owl2',
+				translateY: '-85%'
+			},
+			{
+				element: '.char-owl3',
+				translateY: '-85%'
+			},
+			{
+				element: '.char-owl4',
+				translateY: '-85%'
+			},
+			{
+				element: '.char-owl1 .owl',
+				callback: function(progress){
+					var i, owl, start, end, prgrs, pos, opacity, left, right;
+					for (i = 0; i < owls.length; i++){
+						owl = owls[i];
+						start = i * 10;
+						end = 100 - ((owls.length - (i + 1)) * 10);
+						prgrs = clamp(progress, start, end) - start;
+						pos = easeInOutQuad(prgrs, -100, 100, end - start);
+						opacity = easeInOutQuad(prgrs, 0, 1.5, end - start);
+						left = easeInOutQuad(prgrs, 150, -150, end - start);
+						right = easeInOutQuad(prgrs, -150, 150, end - start);
+						owl.owl[0].style[prefix + 'Transform'] = 'translateY(' + pos + 'px)';
+						owl.owl[0].style.opacity = opacity;
+						owl.wings[0].style[prefix + 'Transform'] = 'rotate(' + left + 'deg)';
+						owl.wings[1].style[prefix + 'Transform'] = 'rotate(' + right + 'deg)';
+					}
+				}
+			},
+			{
+				element: '.desc-character',
+				translateY: '-120%',
+				opacity: [-2, 1]
+			}
+		]
+	},
+	{
+		duration: '100%',
+		actors: [
+			{
+				element: '.char-owl1',
+				translateY: ['-85%', '-175%']
+			},
+			{
+				element: '.char-owl2',
+				translateY: ['-85%', '-175%']
+			},
+			{
+				element: '.char-owl3',
+				translateY: ['-85%', '-175%']
+			},
+			{
+				element: '.char-owl4',
+				translateY: ['-85%', '-175%']
+			},
+			{
+				element: '.desc-character',
+				translateY: ['-120%', '-240%'],
+				opacity: -1
 			}
 		]
 	},
