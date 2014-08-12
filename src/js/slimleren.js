508b958ed6dd062dfdbd89391f16910fd294a127
Chiel Kunkels <chiel@kunkels.me>
2014-08-12 10:50:14 +0200

    slimleren: add explanimation section

--- src/js/slimleren.js
+++ src/js/slimleren.js
@@ -16,6 +16,58 @@ require('kubrick-js')([
 		]
 	},
 	{
+		duration: '100%',
+		actors: [
+			{
+				element: '.expl-bg',
+				translateY: '-50%'
+			},
+			{
+				element: '.expl-screen',
+				translateY: '-135%'
+			},
+			{
+				element: '.expl-book1',
+				translateY: 530
+			},
+			{
+				element: '.expl-book2',
+				translateY: 500
+			},
+			{
+				element: '.expl-book3',
+				translateY: 600
+			},
+			{
+				element: '.expl-glasses',
+				translateY: 450
+			},
+			{
+				element: '.desc-explanimation',
+				translateY: '-120%',
+				opacity: [-2, 1]
+			}
+		]
+	},
+	{
+		duration: '100%',
+		actors: [
+			{
+				element: '.expl-bg',
+				translateY: ['-50%', '-240%']
+			},
+			{
+				element: '.expl-screen',
+				translateY: ['-135%', '-240%']
+			},
+			{
+				element: '.desc-explanimation',
+				translateY: ['-120%', '-240%'],
+				opacity: -1
+			}
+		]
+	},
+	{
 		stage: '.footer-main',
 		duration: 1
 	}
