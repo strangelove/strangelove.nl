b73f10ddbf3b62ed928ca8cc58bc49411573b3e7
Chiel Kunkels <chiel@kunkels.me>
2014-08-12 12:08:11 +0200

    slimleren: add client section

--- src/js/slimleren.js
+++ src/js/slimleren.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var timestamp = new Date().getTime(),
+	phoneReplaced;
+
 require('kubrick-js')([
 	{
 		duration: '75%',
@@ -12,6 +15,95 @@ require('kubrick-js')([
 				element: '.case-summary',
 				translateY: '-40%',
 				opacity: -1
+			},
+			{
+				element: '.desc-client',
+				translateY: '-65%'
+			},
+			{
+				element: '.feather1',
+				translateY: '-80%'
+			},
+			{
+				element: '.feather2',
+				translateY: '-75%'
+			},
+			{
+				element: '.feather3',
+				translateY: '-65%'
+			},
+			{
+				element: '.feather4',
+				translateY: ['5%', '-45%']
+			},
+			{
+				element: '.client-phone',
+				translateY: '-77%'
+			},
+			{
+				element: '.client-phone',
+				callback: function(){
+					if (phoneReplaced) return;
+					phoneReplaced = true;
+					var bgImage = window.getComputedStyle(this).backgroundImage;
+					bgImage = bgImage.replace('-begin', '');
+					bgImage = bgImage.replace(')', '?' + timestamp + ')');
+					this.style.backgroundImage = bgImage;
+				}
+			},
+			{
+				element: '.client-teacher',
+				translateY: '-85%'
+			},
+			{
+				element: '.client-student',
+				translateY: '-75%'
+			},
+			{
+				element: '.client-clock',
+				translateY: '-85%'
+			}
+		]
+	},
+	{
+		duration: '100%',
+		actors: [
+			{
+				element: '.desc-client',
+				translateY: ['-65%', '-185%'],
+				opacity: -1
+			},
+			{
+				element: '.feather1',
+				translateY: ['-80%', '-180%']
+			},
+			{
+				element: '.feather2',
+				translateY: ['-75%', '-180%']
+			},
+			{
+				element: '.feather3',
+				translateY: ['-65%', '-180%']
+			},
+			{
+				element: '.feather4',
+				translateY: ['-45%', '-180%']
+			},
+			{
+				element: '.client-phone',
+				translateY: ['-77%', '-150%']
+			},
+			{
+				element: '.client-teacher',
+				translateY: ['-85%', '-220%']
+			},
+			{
+				element: '.client-student',
+				translateY: ['-75%', '-180%']
+			},
+			{
+				element: '.client-clock',
+				translateY: ['-85%', '-180%']
 			}
 		]
 	},
