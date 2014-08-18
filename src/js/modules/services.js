d18bd7c2b2cc6f375367ba302a51ef9e080b8116
Chiel Kunkels <chiel@kunkels.me>
2014-08-18 14:09:52 +0200

    home: make service icons loop only on icon hover

--- src/js/modules/services.js
+++ src/js/modules/services.js
@@ -6,12 +6,12 @@ var causeLoop = function(el){
 	setTimeout(function(){ el.removeClass('loop'); }, 1720);
 };
 
-$('.services').delegate('mouseover', '.service', function(e, el){
+$('.services').delegate('mouseover', '.icon', function(e, el){
 	if (!el.hasClass('loop')) causeLoop(el);
 });
 
-scrollFx.add({ el: $('.clarify'), posStart: 500, callback: causeLoop });
-scrollFx.add({ el: $('.create'),  posStart: 500, callback: causeLoop });
-scrollFx.add({ el: $('.code'),    posStart: 500, callback: causeLoop });
-scrollFx.add({ el: $('.connect'), posStart: 500, callback: causeLoop });
-scrollFx.add({ el: $('.control'), posStart: 500, callback: causeLoop });
+scrollFx.add({ el: $('.clarify .icon'), posStart: 500, callback: causeLoop });
+scrollFx.add({ el: $('.create .icon'),  posStart: 500, callback: causeLoop });
+scrollFx.add({ el: $('.code .icon'),    posStart: 500, callback: causeLoop });
+scrollFx.add({ el: $('.connect .icon'), posStart: 500, callback: causeLoop });
+scrollFx.add({ el: $('.control .icon'), posStart: 500, callback: causeLoop });
