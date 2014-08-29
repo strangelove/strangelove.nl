b07453ece6225edf9f489e91fdb178902ead8800
Chiel Kunkels <chiel@kunkels.me>
2014-08-29 11:07:08 +0200

    animate "back to top" scroll

--- src/js/modules/back-to-top.js
+++ src/js/modules/back-to-top.js
@@ -1,11 +1,32 @@
 'use strict';
 
 var $ = require('elements'),
-	btn = $('.back-to-top');
+	easeInOutQuad = require('kubrick-js/easing/easeInOutQuad'),
+	getScrollTop = require('kubrick-js/lib/scrolltop'),
+	btn = $('.back-to-top'),
+	windowHeight = window.innerHeight - 1;
 
-var windowHeight = window.innerHeight - 1;
+require('moofx');
 
 $(window).on('scroll', function(){
-	var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-	btn[scrollTop > windowHeight ? 'addClass' : 'removeClass']('shown');
+	btn[getScrollTop() > windowHeight ? 'addClass' : 'removeClass']('shown');
+});
+
+btn.on('click', function(e){
+	e.preventDefault();
+
+	var start = getScrollTop(),
+		change = -start,
+		duration = 150,
+		currentTime = 0,
+		increment = 10;
+
+	var animateScroll = function(){
+		currentTime += increment;
+		window.scrollTo(0, easeInOutQuad(currentTime, start, change, duration));
+		if(currentTime < duration){
+			setTimeout(animateScroll, increment);
+		}
+	};
+	animateScroll();
 });
